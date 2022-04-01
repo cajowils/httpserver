039ca093af7d89c1a4b7d3aead646db37650b474
cajowils <cajowils@ucsc.edu>
2022-03-31 23:20:17 -0700

    error warnings

--- asgn0/split.c
+++ asgn0/split.c
@@ -6,19 +6,24 @@
 
 int main(int argc, char **argv) {
     
+    if (argc < 3) {
+        printf("Not enough arguments\nusage: ./split: <split_char> [<file1> <file2> ...]\n");
+        return 0;
+    }
+
     char *delimiter = argv[1];
 
     //check if delimiter is > 1 char, otherwise use warn or err
 
     if (strlen(delimiter) > 1) {
-        warn("Cannot handle multi-character splits: %s\nusage: ./split: <split_char> [<file1> <file2> ...]", delimiter);
+        printf("Cannot handle multi-character splits: %s\nusage: ./split: <split_char> [<file1> <file2> ...]\n", delimiter);
         return 0;
     }
 
-    //test
     //printf("Delimiter: %lu\n", delimiter);
 
     for (int i=2; i < argc; i++) {
+
         char buffer[100];
         int size = snprintf(buffer, 100, "File: %s\n", argv[i]);
         write(1, buffer, size);
