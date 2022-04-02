508e5b28f58b9935f0db82043feeb87d4babda8f
cajowils <cajowils@ucsc.edu>
2022-04-02 11:30:32 -0700

    added functionality

--- asgn0/split.c
+++ asgn0/split.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <err.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
 
 int main(int argc, char **argv) {
     
@@ -22,23 +26,29 @@ int main(int argc, char **argv) {
     //iterate through files, read them in, and write the version that is split by the delimiter
     for (int i=2; i < argc; i++) {
 
+        int fd = open(argv[i], O_RDONLY);
+
         if (strcmp(argv[i], "-") == 0) {
             printf("read from stdin\n");
         }
-
-        else if (access(argv[i], F_OK) == 0) {
-            printf("file exists\n");
-            char buffer[100];
-            int size = snprintf(buffer, 100, "File: %s\n", argv[i]);
-            write(1, buffer, size);
+        else if (fd != -1) {
+            char *buf = (char *) calloc(100, sizeof(char));
+            int size = read(fd, buf, 1);
+            while (size > 0) {
+                //int size = snprintf(buf, 100, "File: %s\n", argv[i]);
+                if (strcmp(delimiter, buf) == 0) {
+                    write(1, "\n", strlen("\n"));
+                }
+                else {
+                    write(1, buf, size);
+                }
+                size = read(fd, buf, 1);
+            }
+            
         }
         else {
             warn("%s", argv[i]);
         }
-
-        
-
-    //syscall(SYS_write, 1, , 120);
     }
 
     return 0;
