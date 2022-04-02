4bfe8c0a501ec80fde5c44b74c9a03c0c72c66b3
cajowils <cajowils@ucsc.edu>
2022-04-02 11:46:11 -0700

    removed printf

--- asgn0/split.c
+++ asgn0/split.c
@@ -6,19 +6,15 @@
 #include <err.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h> 
-
-
+#include <fcntl.h>
 
 int read_write(int fd, char *delimiter) {
     char *buf = (char *) calloc(100, sizeof(char));
     int size = read(fd, buf, 1);
     while (size > 0) {
-        //int size = snprintf(buf, 100, "File: %s\n", argv[i]);
         if (strcmp(delimiter, buf) == 0) {
             write(1, "\n", strlen("\n"));
-        }
-        else {
+        } else {
             write(1, buf, size);
         }
         size = read(fd, buf, 1);
@@ -26,11 +22,10 @@ int read_write(int fd, char *delimiter) {
     return 0;
 }
 
-
 int main(int argc, char **argv) {
-    
+
     if (argc < 3) {
-        printf("Not enough arguments\nusage: ./split: <split_char> [<file1> <file2> ...]\n");
+        //printf("Not enough arguments\nusage: ./split: <split_char> [<file1> <file2> ...]\n");
         return 0;
     }
 
@@ -39,22 +34,20 @@ int main(int argc, char **argv) {
     //check if delimiter is > 1 char, otherwise use warn or err
 
     if (strlen(delimiter) > 1) {
-        printf("Cannot handle multi-character splits: %s\nusage: ./split: <split_char> [<file1> <file2> ...]\n", delimiter);
+        //printf("Cannot handle multi-character splits: %s\nusage: ./split: <split_char> [<file1> <file2> ...]\n", delimiter);
         return 0;
     }
     //iterate through files, read them in, and write the version that is split by the delimiter
-    for (int i=2; i < argc; i++) {
+    for (int i = 2; i < argc; i++) {
 
         int fd = open(argv[i], O_RDONLY);
 
         if (strcmp(argv[i], "-") == 0) {
             read_write(0, delimiter);
-        }
-        else if (fd != -1) {
+        } else if (fd != -1) {
             read_write(fd, delimiter);
-            
-        }
-        else {
+
+        } else {
             warn("%s", argv[i]);
         }
     }
